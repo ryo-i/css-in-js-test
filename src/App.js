2407cb584bec6e96e073130fa68e03de7f53162a
ryo-i <iidazz0109@gmail.com>
2020-12-16 05:01:51 +0900

    ファイル一式をアップ

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Block = styled.div`
+  margin: 20px;
+  padding: 0 20px;
+  background: #333;
+  border-radius: 10px;
+`;
+
+const CssInJsTest = styled.p`
+  color: red;
+`;
+
+const keyColor = 'yellow';
+
+const CssInJsTest2 = styled.p`
+  color: ${keyColor};
+  &:hover {
+    background: rgba(255,255,255,0.3);
+  }
+  & span {
+    font-weight: bold;
+  }
+`;
 
 function App() {
   return (
@@ -17,6 +41,14 @@ function App() {
         >
           Learn React
         </a>
+        <Block>
+          <CssInJsTest>
+            CSS in JSテストですと。
+          </CssInJsTest>
+          <CssInJsTest2>
+            CSS in JS<span>テスト2</span>ですと。
+          </CssInJsTest2>
+        </Block>
       </header>
     </div>
   );
